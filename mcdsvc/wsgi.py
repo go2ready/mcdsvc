9060452837310068772dc62a71862a8fa471415c
Shiwei Wang <sw3812@ic.ac.uk>
2019-04-07 20:29:20 +0100

    Update wsgi

--- mcdsvc/wsgi.py
+++ mcdsvc/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/mcdsvc')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/mcdsvc/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mcdsvc.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcdsvc.settings')
 
 application = get_wsgi_application()
