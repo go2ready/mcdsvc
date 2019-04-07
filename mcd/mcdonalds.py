7f8054351748961bfad230d42c88d7a1ad06e8a3
Shiwei Wang <sw3812@ic.ac.uk>
2019-04-07 22:49:10 +0100

    add chrome drive

--- mcd/mcdonalds.py
+++ mcd/mcdonalds.py
@@ -9,7 +9,7 @@ def retrieveCode(code1, code2, code3, pound, pence):
     POUND=pound
     PENCE=pence
 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path="/opt/bitnami/apps/django/django_projects/chromedriver")
     #driver = webdriver.Firefox(executable_path=r'/opt/bitnami/apps/django/django_projects/geckodriver', log_path="/opt/bitnami/apps/django/django_projects/geckodriver.log")
     try:
         driver.get('https://www.mcdfoodforthoughts.com/')
