023b4ea1456f05ddfe2dde163879531a67f5bdd4
Shiwei Wang <sw3812@ic.ac.uk>
2019-04-07 21:07:49 +0100

    update build repo

---
+++ mcd/build/precache-manifest.bde92ee62efee1ac9f764510b5e3bbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc618c256460f39520444271f397fb64",
+    "url": "/static/media/demo.dc618c25.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "895cda0186f1e7a37928",
+    "url": "/static/js/main.61d9b7f5.chunk.js"
+  },
+  {
+    "revision": "e6ae3cbe2bd34e7b7679",
+    "url": "/static/js/2.f52a0ba3.chunk.js"
+  },
+  {
+    "revision": "895cda0186f1e7a37928",
+    "url": "/static/css/main.e1178d68.chunk.css"
+  },
+  {
+    "revision": "43b009bd0fa5dfa858c1aed00fddd64c",
+    "url": "/index.html"
+  }
+];
