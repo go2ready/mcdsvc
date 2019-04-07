57f1ca3494efe656f27912eb5ea83165612e1d0c
Shiwei Wang <sw3812@ic.ac.uk>
2019-04-07 22:11:30 +0100

    Add log path

--- mcd/mcdonalds.py
+++ mcd/mcdonalds.py
@@ -9,7 +9,7 @@ def retrieveCode(code1, code2, code3, pound, pence):
     POUND=pound
     PENCE=pence
 
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(log_path="/opt/bitnami/apps/django/django_projects/geckodriver.log")
     try:
         driver.get('https://www.mcdfoodforthoughts.com/')
 
