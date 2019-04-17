a0fc82e55aa47674eae105faefca0cc8fd582f90
Shiwei Wang <sw3812@ic.ac.uk>
2019-04-17 21:01:37 +0100

    Move index and foornote

--- mcd/src/components/UserControl/FootNote.tsx
+++ mcd/src/components/UserControl/FootNote.tsx
@@ -69,7 +69,7 @@ export const FootNote = withStyles(styles)(
             THE SERVICE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SERVICE OR THE USE OR OTHER DEALINGS IN THE SERVICE.
           </Typography>
           <Typography variant="body1" gutterBottom>
-            <b>Idendity verified by <a href="https://www.wosign.com/english/index.htm"><img src={wotrus} alt="wotrus" /></a> Your data is encrypted to the highest standard.</b>
+            <b>Identity verified by <a href="https://www.wosign.com/english/index.htm"><img src={wotrus} alt="wotrus" /></a> Your data is encrypted to the highest standard.</b>
           </Typography>
         </div>
       );
